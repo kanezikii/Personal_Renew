a2a7ce73d1598c6af48d5b15e778a055c3ed0906
kanezikii <peng320829@gmail.com>
2026-09-19 13:41:22 +0800

    Update pingless.py

--- pingless.py
+++ pingless.py
@@ -5,10 +5,10 @@ import os, re, sys, time, json, requests, urllib
 from seleniumbase import SB
 
 # 环境变量配置(可以直接私库在双引号里填写)
-EMAIL         = os.environ.get("EMAIL") or ""           # 邮箱,只用于通知使用，可随意填写
-DISCORD_TOKEN = os.environ.get("pingDISCORD_TOKEN") or ""   # Discord Token 备用登录方式, 失败时才使用,必须填写
-TG_CHAT_ID    = os.environ.get("TG_CHAT_ID") or ""      # TG chat id,不填写不通知，需和bot token一起填写生效
-TG_BOT_TOKEN  = os.environ.get("TG_BOT_TOKEN") or ""    # TG bot token 
+EMAIL         = os.environ.get("EMAIL") or ""
+DISCORD_TOKEN = os.environ.get("pingDISCORD_TOKEN") or ""
+TG_CHAT_ID    = os.environ.get("TG_CHAT_ID") or ""
+TG_BOT_TOKEN  = os.environ.get("TG_BOT_TOKEN") or ""
 
 BASE_URL = "https://dash.pingless.org"
 
